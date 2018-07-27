42329e0fd334fdd1f757db392379fc4e6ec9b833
e.guskov <e.guskov@gaijin.ru>
2018-07-27 21:13:16 +0300

    add missed file.

---
+++ ecs/components/default.component.cpp
@@ -0,0 +1,5 @@
+#include "default.component.h"
+
+REG_COMP_INIT(glm::vec2, vec2);
+REG_COMP_INIT(glm::vec3, vec3);
+REG_COMP_ARR_INIT(glm::vec2, vec2, 2);
---
+++ ecs/components/default.component.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ecs/component.h"
+
+template <>
+struct Setter<int>
+{
+  static inline bool set(int &v, const JValue &value)
+  {
+    assert(value.IsInt());
+    v = value.GetInt();
+    return true;
+  }
+};
+
+template <>
+struct Setter<glm::vec2>
+{
+  static inline bool set(glm::vec2 &vec, const JValue &value)
+  {
+    assert(value.HasMember("x"));
+    assert(value.HasMember("y"));
+    vec.x = value["x"].GetFloat();
+    vec.y = value["y"].GetFloat();
+    return true;
+  }
+};
+
+template <>
+struct Setter<glm::vec3>
+{
+  static inline bool set(glm::vec3 &vec, const JValue &value)
+  {
+    assert(value.HasMember("x"));
+    assert(value.HasMember("y"));
+    assert(value.HasMember("z"));
+    vec.x = value["x"].GetFloat();
+    vec.y = value["y"].GetFloat();
+    vec.z = value["z"].GetFloat();
+    return true;
+  }
+};
+
+REG_COMP(glm::vec2, vec2);
+REG_COMP(glm::vec3, vec3);
+REG_COMP_ARR(glm::vec2, vec2, 2);
