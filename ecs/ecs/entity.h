c2a274e3d063d1d345925cc83b68781747f2165f
e.guskov <e.guskov@gaijin.ru>
2019-03-21 21:34:17 +0300

    ECS: Add operator!= to EntityId

--- ecs/ecs/entity.h
+++ ecs/ecs/entity.h
@@ -16,6 +16,7 @@ struct EntityId
 
   EntityId(uint32_t h = 0xFFFFFFFF) : handle(h) {}
   bool operator==(const EntityId &rhs) const { return handle == rhs.handle; }
+  bool operator!=(const EntityId &rhs) const { return handle != rhs.handle; }
 };
 
 inline EntityId make_eid(uint16_t gen, uint16_t index)
