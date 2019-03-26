a1089759bdeb33fd48d9044df681179dbec10f64
e.guskov <e.guskov@gaijin.ru>
2019-03-26 13:02:47 +0300

    BENCHMARK: Test JobManger performance

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -2,6 +2,10 @@
 #include <ecs/jobmanager.h>
 #include <ecs/perf.h>
 
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "benchmark-update.h"
 
 PULL_ESC_CORE;
@@ -41,15 +45,90 @@ static constexpr ConstQueryDesc update_position_query_desc = {
   empty_desc_array,
 };
 
+template <int MEASURE_COUNT>
+struct PerfMeasure
+{
+  static constexpr double from = 0.;
+  static constexpr double to = 100000.;
+  static constexpr double step = 5.;
+  static constexpr int freqCount = (int)(((to - from) / step) + 0.5);
+
+  int *freq = nullptr;
+
+  double minTime = -1.0;
+  double maxTime = -1.0;
+
+  time_point startTotal;
+  time_point lastMeasure;
+
+  eastl::string name;
+
+  PerfMeasure(const char *_name) : name(_name)
+  {
+    freq = new int[freqCount];
+    ::memset(freq, 0, sizeof(int) * freqCount);
+
+    startTotal = std::chrono::high_resolution_clock::now();
+  }
+
+  ~PerfMeasure()
+  {
+    std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - startTotal;
+    const double d = diff.count();
+
+    int sumFreq = 0;
+    int maxFreq = -1;
+    int maxFreqIndex = -1;
+    for (int i = 0; i < freqCount; ++i)
+    {
+      sumFreq += freq[i];
+      if (freq[i] > maxFreq)
+      {
+        maxFreq = freq[i];
+        maxFreqIndex = i;
+      }
+    }
+
+    printf("[%s]: %6.2f ms; min: %4.2f ms; max: %5.2f ms; avg: %3d us; mean: %5.1f us (%d/%d);\n",
+      name.c_str(),
+      d,
+      minTime * 1e-3, maxTime * 1e-3,
+      int((d / float(MEASURE_COUNT)) * 1000.f),
+      double(maxFreqIndex) * step,
+      maxFreq, sumFreq);
+
+    delete[](freq);
+  }
+
+  void startMeasure()
+  {
+    lastMeasure = std::chrono::high_resolution_clock::now();
+  }
+
+  void stopMeasure()
+  {
+    std::chrono::duration<double, std::micro> diff = std::chrono::high_resolution_clock::now() - lastMeasure;
+    const double d = diff.count();
+    if (d < minTime || minTime < 0.0)
+      minTime = d;
+    if (d > maxTime || maxTime < 0.0)
+      maxTime = d;
+
+    int index = (int)::floor(d / step);
+    if (index >= freqCount)
+      index = freqCount - 1;
+    if (index < 0)
+      index = 0;
+    ++freq[index];
+  }
+};
+
 int main()
 {
   using namespace std::chrono_literals;
 
   EntityManager::create();
 
-  // TODO: 1. Entites
-  // TODO: 2. Native loop
-  // TODO: 3. Script loop
   const int count = 50000;
 
   eastl::vector<Test> test;
@@ -80,94 +159,48 @@ int main()
 
   {
     PERF_TIME(Create);
-    for (int i = 0; i < count; ++i)
-      g_mgr->createEntity("test", JValue());
-    g_mgr->tick();
+    for (int j = 0; j < 1; ++j)
+    {
+      for (int i = 0; i < count; ++i)
+        g_mgr->createEntity("test", JValue());
+      g_mgr->tick();
+      clear_frame_mem();
+    }
   }
 
   const float dt = 1.f / 60.f;
 
+  static const int MEASURE_COUNT = 1000;
+
   {
-    // PERF_TIME(JobManager64);
+    PerfMeasure<MEASURE_COUNT> perf("Native   ");
 
-    const double from = 0.;
-    const double to = 100.;
-    const double step = 0.01;
-    int freqCount = (int)::ceil((to - from) / step);
-    int *freq = new int[freqCount];
-    ::memset(freq, 0, sizeof(int) * freqCount);
-
-    double minTime = -1.0;
-    double maxTime = -1.0;
-    time_point start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 1000; ++i)
+    for (int i = 0; i < MEASURE_COUNT; ++i)
     {
-      time_point s = std::chrono::high_resolution_clock::now();
-      jobmanager::add_job(64, count,
+      perf.startMeasure();
+      for (auto &v : test)
+        v.pos += v.vel * dt;
+      perf.stopMeasure();
+    }
+  }
+
+  {
+    PerfMeasure<MEASURE_COUNT> perf("JM Native");
+    for (int i = 0; i < MEASURE_COUNT; ++i)
+    {
+      perf.startMeasure();
+      jobmanager::add_job(count,
         [&](int from, int count)
         {
-          // DEBUG_LOG("From: " << from << "; Count: " << count);
           auto b = testJobManager.begin() + from;
-          auto e = testJobManager.begin() + from + count;
+          auto e = b + count;
           for (; b != e; ++b)
             (*b).pos += (*b).vel * dt;
         });
       jobmanager::do_and_wait_all_tasks_done();
-
-      std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - s;
-      const double d = diff.count();
-      if (d < minTime || minTime < 0.0)
-        minTime = d;
-      if (d > maxTime || maxTime < 0.0)
-        maxTime = d;
-
-      int index = (int)::floor(d / step);
-      ++freq[index];
+      perf.stopMeasure();
     }
-
-    std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - start;
-    const double d = diff.count();
-
-    int maxFreq = -1;
-    for (int i = 0; i < freqCount; ++i)
-      if (freq[i] > maxFreq)
-        maxFreq = freq[i];
-
-    std::cout << "[" << "JobManager64" << "]: " << d << " ms " << std::endl;
-    std::cout << "[" << "JobManager64" << "]: min: " << minTime << " ms " << std::endl;
-    std::cout << "[" << "JobManager64" << "]: max: " << maxTime << " ms " << std::endl;
-    std::cout << "[" << "JobManager64" << "]: agv: " << (d / 1000.f) << " ms " << std::endl;
-    std::cout << "[" << "JobManager64" << "]: mean: " << (double(maxFreq) * step) << " ms " << std::endl;
   }
-
-  //{
-  //  // PERF_TIME(Native);
-  //  double minTime = -1.0;
-  //  double maxTime = -1.0;
-  //  time_point start = std::chrono::high_resolution_clock::now();
-
-  //  for (int i = 0; i < 1000; ++i)
-  //  {
-  //    time_point s = std::chrono::high_resolution_clock::now();
-
-  //    for (auto &v : test)
-  //      v.pos += v.vel * dt;
-
-  //    std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - s;
-  //    const double d = diff.count();
-  //    if (d < minTime || minTime < 0.0)
-  //      minTime = d;
-  //    if (d > maxTime || maxTime < 0.0)
-  //      maxTime = d;
-  //  }
-
-  //  std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - start;
-  //  const double d = diff.count();
-  //  std::cout << "[" << "Native" << "]: " << d << " ms " << std::endl;
-  //  std::cout << "[" << "Native" << "]: min: " << minTime << " ms " << std::endl;
-  //  std::cout << "[" << "Native" << "]: max: " << maxTime << " ms " << std::endl;
-  //  std::cout << "[" << "Native" << "]: agv: " << (d / 1000.f) << " ms " << std::endl;
-  //}
 
   {
     auto stage = UpdateStage{ dt, 10.f };
@@ -176,51 +209,62 @@ int main()
     query.desc = update_position_query_desc;
     g_mgr->performQuery(query);
 
-    // PERF_TIME(ECS);
-    double minTime = -1.0;
-    double maxTime = -1.0;
-    time_point start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 1000; ++i)
+    PerfMeasure<MEASURE_COUNT> perf("JM ECS   ");
+    for (int i = 0; i < MEASURE_COUNT; ++i)
     {
-      time_point s = std::chrono::high_resolution_clock::now();
-      // g_mgr->tick(UpdateStage{ dt, 10.f });
+      perf.startMeasure();
+      jobmanager::add_job(query.entitiesCount,
+        [&](int from, int count)
+        {
+          auto begin = query.begin(from);
+          auto end = query.begin(from + count);
+          for (auto q = begin, e = end; q != e; ++q)
+            update_position::run(*(UpdateStage*)&stage,
+              GET_COMPONENT(update_position, q, glm::vec3, vel),
+              GET_COMPONENT(update_position, q, glm::vec3, pos));
+        });
+      jobmanager::do_and_wait_all_tasks_done();
+      perf.stopMeasure();
+    }
+  }
+
+  {
+    auto stage = UpdateStage{ dt, 10.f };
+
+    Query query;
+    query.desc = update_position_query_desc;
+    g_mgr->performQuery(query);
+
+    PerfMeasure<MEASURE_COUNT> perf("ECS      ");
+    for (int i = 0; i < MEASURE_COUNT; ++i)
+    {
+      perf.startMeasure();
       for (auto q = query.begin(), e = query.end(); q != e; ++q)
         update_position::run(*(UpdateStage*)&stage,
           GET_COMPONENT(update_position, q, glm::vec3, vel),
           GET_COMPONENT(update_position, q, glm::vec3, pos));
-
-      std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - s;
-      const double d = diff.count();
-      if (d < minTime || minTime < 0.0)
-        minTime = d;
-      if (d > maxTime || maxTime < 0.0)
-        maxTime = d;
+      perf.stopMeasure();
     }
-
-    std::chrono::duration<double, std::milli> diff = std::chrono::high_resolution_clock::now() - start;
-    const double d = diff.count();
-    std::cout << "[" << "ECS" << "]: " << d << " ms " << std::endl;
-    std::cout << "[" << "ECS" << "]: min: " << minTime << " ms " << std::endl;
-    std::cout << "[" << "ECS" << "]: max: " << maxTime << " ms " << std::endl;
-    std::cout << "[" << "ECS" << "]: agv: " << (d / 1000.f) << " ms " << std::endl;
   }
 
-  //{
-  //  auto stage = UpdateStage{ dt, 10.f };
+  {
+    auto stage = UpdateStage{ dt, 10.f };
 
-  //  Query query;
-  //  query.desc = update_position_query_desc;
-  //  g_mgr->performQuery(query);
+    Query query;
+    query.desc = update_position_query_desc;
+    g_mgr->performQuery(query);
 
-  //  PERF_TIME(ECS_FOREACH);
-  //  for (int i = 0; i < 1000; ++i)
-  //  {
-  //    for (auto q : query)
-  //      update_position::run(*(UpdateStage*)&stage,
-  //        GET_COMPONENT(update_position, q, glm::vec3, vel),
-  //        GET_COMPONENT(update_position, q, glm::vec3, pos));
-  //  }
-  //}
+    PerfMeasure<MEASURE_COUNT> perf("ECS Feach");
+    for (int i = 0; i < MEASURE_COUNT; ++i)
+    {
+      perf.startMeasure();
+      for (auto q : query)
+        update_position::run(*(UpdateStage*)&stage,
+          GET_COMPONENT(update_position, q, glm::vec3, vel),
+          GET_COMPONENT(update_position, q, glm::vec3, pos));
+      perf.stopMeasure();
+    }
+  }
 
   std::cin.get();
 
