15c6d959e5266f7366fcd0a2da71a18ffe19d8f5
e.eguskov <e.guskov@gaijin.ru>
2019-05-12 22:49:06 +0300

    Sample: Fix grid
    Was broken while work on Boids

--- sample/grid.cpp
+++ sample/grid.cpp
@@ -7,16 +7,8 @@
 
 struct update_grid_cell
 {
-  ECS_RUN(const UpdateStage &stage, const glm::vec2 &cur_pos, int &grid_cell)
+  ECS_RUN(const UpdateStage &stage, const glm::vec2 &pos, int &grid_cell)
   {
-    grid_cell = MAKE_GRID_CELL_FROM_POS(cur_pos, GRID_CELL_SIZE);
-  }
-};
-
-struct update_small_grid_cell
-{
-  ECS_RUN(const UpdateStage &stage, const glm::vec2 &cur_pos, int &small_grid_cell)
-  {
-    small_grid_cell = MAKE_GRID_CELL_FROM_POS(cur_pos, SMALL_GRID_CELL_SIZE);
+    grid_cell = MAKE_GRID_CELL_FROM_POS(pos, GRID_CELL_SIZE);
   }
 };
